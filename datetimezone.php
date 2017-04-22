e7f02f392f175c5c91fc2144a4a7e7570f7fc901
manoj <watermark0913@gmail.com>
2017-04-22 12:51:06 +0530

    firstcommit

---
+++ datetimezone.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set("Asia/Kolkata");
+echo "Indian Time:-".date("Y-m-d H:i:s");
+
+date_default_timezone_set("America/New_York");
+echo "<br/>American Time:-".date("Y-m-d H:i:s");
+?>
