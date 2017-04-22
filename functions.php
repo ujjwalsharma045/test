be9dfabec59ed989af32a544738abbad2f11f006
manoj <watermark0913@gmail.com>
2017-04-22 12:57:12 +0530

    second move

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+$inputString = "xyz123";
+echo "</br>Wrong way</br>";
+//bad code:
+if(strpos($inputString, 'xyz') == false){
+    echo "string does not exists";
+}
+else {
+	echo "string exists";
+}
+
+echo "</br>Right way</br>";
+//good code:
+if (strpos($inputString, 'xyz') === false) {
+	echo "string does not exists";	    
+}
+else {
+	echo "string exists";
+}
+
+
+$anotherString = "3";
+echo "</br>Wrong way</br>";
+//bad code:
+if($anotherString == 3){
+    echo "true";
+}
+else {
+	echo "false";
+}
+
+echo "</br>Right way</br>";
+//good code:
+if ($anotherString === 3) {
+	echo "true";	    
+}
+else {
+	echo "false";
+}
+?>
